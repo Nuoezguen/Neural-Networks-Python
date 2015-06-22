c4f47accca944ea80b353a7dc3d10e7fa5b9e1be
tdpetrou <petrou.theodore@gmail.com>
2015-06-22 18:10:59 -0500

    added multiclass dimensionality

--- neural_network.py
+++ neural_network.py
@@ -11,19 +11,17 @@ Give choice for using advanced optimization or SGD
 Should work for any amount of output layers
 Stop training when error not changing
 Handle overflow and log(0) numerical stuff
+Add regression functionality
 
 '''
 
-
-import matplotlib
-# matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import numpy as np
-
+from scipy import optimize
+import matplotlib.pyplot as plt
 
 class Neural_Network():
     
-    def __init__(self, layer_sizes, random_seed=None, learning_rate=1):
+    def __init__(self, layer_sizes, random_seed=None, learning_rate=1, opt=True):
         '''
         Initialize Neural network
 
@@ -57,6 +55,7 @@ class Neural_Network():
         self.layer_sizes = layer_sizes
         self.learning_rate = learning_rate
         self.__stoping_threshold = 1e-13
+        self.opt = opt
         
         #Can set to repeat results
         if random_seed is not None:
@@ -147,7 +146,7 @@ class Neural_Network():
         # Remove column of ones in last layer  
         self.__a[-1] = self.__a[-1][:,1:]
     
-    def back_prop(self, y):
+    def back_prop(self):
         '''
         Backpropagation algorithm. Attempts to find "error" in each layer and use SGD
         to move weights closer to optimum
@@ -169,14 +168,53 @@ class Neural_Network():
         #Gets error in last layer first
         # Do not need to multiply by sigmoid gradient if using MLE as cost function
         # for better explanation http://neuralnetworksanddeeplearning.com/chap2.html
-        self.__deltas[-1] = np.multiply(self.__a[-1] - y, self.__sigmoid_gradient(self.__a[-1]))
+        
+        # Using error function E = (y' - y) ^ 2
+        self.__deltas[-1] = np.multiply(self.__a[-1] - self.__y_encoded_mat, self.__sigmoid_gradient(self.__a[-1]))
 
         for i in range(-1, -len(self.__deltas), -1):
             self.__deltas[i-1] = np.multiply(np.dot(self.__deltas[i], self.weights[i].T), self.__sigmoid_gradient(self.__a[i-1]))[:, 1:]
 
         for i, D in enumerate(self.__DELTAS):
             self.__DELTAS[i] = np.dot(self.__a[i].T, self.__deltas[i])
-            self.weights[i] -= self.learning_rate * self.__DELTAS[i] / len(y)
+            self.weights[i] -= self.learning_rate * self.__DELTAS[i] / len(self.__y_encoded_mat)
+    
+    def cost_func_opt(self, x, *args):
+        cum_sum = [0]
+        for i, layer in enumerate(self.layer_sizes[:-2]):
+
+            cum_sum.append((layer + 1) * self.layer_sizes[i + 1])
+            print cum_sum
+            print (self.layer_sizes[i + 1] + 1, layer)
+            self.weights[i] = x[cum_sum[-2]:cum_sum[-1]].reshape(self.layer_sizes[i + 1] + 1, layer)
+        
+        self.weights[-1] = x[cum_sum[-1]:].reshape(self.layer_sizes[i + 1] + 1, self.layer_sizes[i + 2])
+ 
+        self.feed_forward(X)
+        
+        return 1./len(y) * np.sum((y - self.__a[-1]) ** 2) / 2
+    
+    def gradf(self, x, *args):
+        cum_sum = [0]
+        for i, layer in enumerate(self.layer_sizes[:-2]):
+
+            cum_sum.append((layer + 1) * self.layer_sizes[i + 1])
+            print cum_sum
+            print (self.layer_sizes[i + 1] + 1, layer)
+            self.weights[i] = x[cum_sum[-2]:cum_sum[-1]].reshape(self.layer_sizes[i + 1] + 1, layer)
+        
+        self.weights[-1] = x[cum_sum[-1]:].reshape(self.layer_sizes[i + 1] + 1, self.layer_sizes[i + 2])
+ 
+            
+        self.feed_forward(X)
+        self.back_prop(y)
+        
+        ALL_DELTAS = self.__DELTAS[0].ravel()
+        for D in self.__DELTAS[1:]:
+            ALL_DELTAS = np.r_[ALL_DELTAS, D.ravel()]
+        print ALL_DELTAS
+        return ALL_DELTAS
+        
     
     def cost_function(self, y, h, m):
         '''
@@ -208,7 +246,6 @@ class Neural_Network():
         -------
         Squared error divided by two
         '''
-
         return 1./m * np.sum((y - h) ** 2) / 2
         
     def predict(self, X):
@@ -227,9 +264,35 @@ class Neural_Network():
         for i, theta in enumerate(self.weights):
             z = np.dot(a, theta)
             a = np.c_[np.ones(len(z)), self.__sigmoid(z)]
-            
-        return a[:,1:]
         
+        if len(self.classes_) > 2:
+            soft_max = np.argmax(a[:,1:], axis=1)
+            reverse_lookup = {v:k for k,v in self.__encoded_classes.iteritems()}
+            returned_prediction = np.array([reverse_lookup[x] for x in soft_max])
+        else:
+            returned_prediction = np.round(a[:,1:])
+            
+        return returned_prediction
+    
+    def __encode_y_values(self, y):
+        '''
+        Encodes numeric and string values 
+        '''
+        if np.ndim(y) == 2 and y.shape[1] != 1:
+            raise ValueError("bad y shape {0}. Make y 1 dimension or have 1 as its second dimension".format(y.shape))
+        
+        self.classes_ = np.unique(y)
+        self.__encoded_classes = {cls:i for i, cls in enumerate(self.classes_)}
+        
+        if type(y) == np.ndarray:
+            y = y.ravel()
+        self.__y_encoded = np.atleast_2d(np.array([[self.__encoded_classes[cls] for cls in y]]).T)
+        
+        self.__y_encoded_mat = self.__y_encoded.copy()
+        if len(self.classes_) > 2:
+            self.__y_encoded_mat = np.zeros((len(self.__y_encoded), len(self.classes_)))
+            for i, y_encoded in enumerate(self.__y_encoded):
+                self.__y_encoded_mat[i, y_encoded] = 1
         
     def fit(self, X, y):
         '''
@@ -248,19 +311,34 @@ class Neural_Network():
         -----
         Can now predict
         '''
+        if len(X) != len(y):
+            raise ValueError('X and y have incompatible shapes. X has ' + str(len(X)) + ' examples but ' +
+                             'y has ' + str(len(y)) + '.')
+        
+        
+            
+        self.__encode_y_values(y)
 
         self.__a[0] = np.c_[np.ones(len(X)), X]
-    
-        for i in range(50000):
-            self.feed_forward(X)
-            
-            current_cost = self.cost_function_se(y, self.__a[-1], len(y))
-            self.cost.append(current_cost)
-            if abs(self.cost[-1] - self.cost[-2]) < self.__stoping_threshold:
-                print "cost not decreasing", i
-                break
-    
-            self.back_prop(y)
+        
+        if self.opt:
+            unraveled_thetas = self.weights[0].ravel()
+            for theta in self.weights[1:]:
+                unraveled_thetas = np.r_[unraveled_thetas, theta.ravel()]
+            theta_opt,min_val,c,d, e = optimize.fmin_cg(self.cost_func_opt, fprime=self.gradf, x0 = unraveled_thetas, args = (X, y, m), full_output=1,gtol=1e-10 )
+
+        else:
+            for i in range(50000):
+                self.feed_forward(X)
+
+                current_cost = self.cost_function_se(self.__y_encoded_mat, self.__a[-1], len(self.__y_encoded_mat))
+        
+                self.cost.append(current_cost)
+                if abs(self.cost[-1] - self.cost[-2]) < self.__stoping_threshold:
+                    print "cost not decreasing", i
+                    break
+
+                self.back_prop()
 
     def score(self, X, y):
         '''
@@ -275,7 +353,8 @@ class Neural_Network():
         -------
         Accuracy (float)
         '''
-        return np.mean(np.round(self.predict(X)) == y)
+        return np.mean(self.predict(X) == y)
+
 
 if __name__ == '__main__':
     # # xor exmaple
@@ -288,7 +367,7 @@ if __name__ == '__main__':
     X = np.random.rand(300,2)
     y = np.atleast_2d(1 * np.logical_or((X[:,0] - .8) ** 2 + (X[:,1]-.5) ** 2 < .03, (X[:,0] - .2) ** 2 + (X[:,1]-.8) ** 2 < .03)).T
 
-    nn = Neural_Network([2,10,1], learning_rate=5)
+    nn = Neural_Network([2,10,1], learning_rate=1, opt=False)
     nn.fit(X,y)
 
     xx, yy = np.meshgrid(np.linspace(0,1,100), np.linspace(0,1,100))
